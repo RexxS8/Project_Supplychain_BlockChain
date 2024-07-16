cd3c1638a562a59a0a159707b9a555a504aec579
Andri Martin <113878679+RexxS8@users.noreply.github.com>
2024-07-16 15:37:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("JavaScript is successfully linked!");
+
+    // Example: Dynamic content change
+    const header = document.querySelector(".stHeader");
+    if (header) {
+        header.innerHTML = "Updated Transaction Details Header";
+    }
+
+    // Additional JavaScript functionalities can be added here
+});

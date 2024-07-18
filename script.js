da078abc9cf7e79be2176ddaaf8ed96741894db0
Andri Martin <113878679+RexxS8@users.noreply.github.com>
2024-07-18 14:47:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+// JavaScript untuk memastikan scroll horizontal
 document.addEventListener("DOMContentLoaded", function() {
     console.log("JavaScript is successfully linked!");
 
-    // Example: Dynamic content change
-    const header = document.querySelector(".stHeader");
-    if (header) {
-        header.innerHTML = "Updated Transaction Details Header";
+    const transactionDetails = document.querySelector(".stTransactionDetails");
+
+    // Memastikan transaksi yang baru dimuat dapat dilihat dengan scroll horizontal
+    if (transactionDetails) {
+        transactionDetails.addEventListener("DOMSubtreeModified", function() {
+            transactionDetails.scrollLeft = transactionDetails.scrollWidth;
+        });
     }
 
-    // Additional JavaScript functionalities can be added here
+    // Tambahan: Scroll ke kanan saat pengguna memilih opsi yang berbeda
+    const selectBox = document.querySelector(".stSelectbox");
+    if (selectBox) {
+        selectBox.addEventListener("change", function() {
+            transactionDetails.scrollLeft = transactionDetails.scrollWidth;
+        });
+    }
 });
